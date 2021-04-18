253ce8edaa0ff347da6a2a2c8dff37fb414dcf28
Kamesuta <kamesuta@gmail.com>
2021-04-18 19:39:02 +0900

    Setup

---
+++ src/main/java/net/kunmc/lab/homeroom/Homeroom.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.homeroom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Homeroom extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
